7c16dfb6f4b7e8bb80b5d1473e82a9557ab77eb0
deepanshu <deepanshuagarwal9999@gmail.com>
2023-08-13 22:39:45 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,17 +7,17 @@ const Header = () => {
         <img
           src="https://about.netflix.com/images/logo.png"
           alt=""
-          className="w-[200px]"
+          className="sm:w-[200px] w-[120px]"
         />
         <div className="flex items-center gap-5">
-          <div className="border-[1px] py-1  border-white/40 px-2 rounded-sm">
+          <div className="border-[1px] py-1 sm:text-md text-sm border-white/40 px-2 rounded-sm">
             <span>🌐</span>
             <select className="text-white bg-transparent outline-none text-center ">
               <option className="bg-black "> English</option>
               <option className=" bg-black "> हिन्दी</option>
             </select>
           </div>
-          <Link className="block px-4 py-[8px] text-center text-sm font-[600] rounded-[4px] text-white bg-[#E50914]  hover:opacity-90 duration-100">
+          <Link className="block sm:px-4 sm:py-[7px] px-[3px] py-[6px] text-center text-sm font-[600] rounded-[4px] text-white bg-[#E50914]  hover:opacity-90 duration-100">
             Sign in
           </Link>
         </div>
